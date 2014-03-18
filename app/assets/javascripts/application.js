9727777d11a65ad23e225a87aaeafc0cadec59f3
Hieu Dang <dang.hoang.hieu@framgia.com>
2014-03-18 17:50:52 +0700

    finish T11 question ajax and index filter on citeria

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,17 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
+
+$(function(){
+  $("#add-answer").click(function(){
+    checkboxes = $('form .checkbox');
+    correct_answer = $("<input name='' type='hidden' value='0'>");
+    correct_answer.attr('name', 'question[answers_attributes][' +  checkboxes.length + '][correct_answer]');
+    checkbox = $("<input name='' type='checkbox' value='1'>");
+    checkbox.attr('name', 'question[answers_attributes][' +  checkboxes.length + '][correct_answer]');
+    text = $("<input name='' type='text'>");
+    text.attr('name', 'question[answers_attributes][' +  checkboxes.length + '][answer]');
+    label = $('<label class="checkbox">').append(correct_answer).append(checkbox).append(text);
+    $('form #answers-list').append(label);
+  })
+})
